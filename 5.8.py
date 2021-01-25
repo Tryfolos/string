9dd88c4fc4240a15daa5eff4892451f9eea5a760
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2021-01-25 13:08:34 +0100

    change

---
+++ 5.8.py
@@ -0,0 +1,7 @@
+#This script takes a text/string as input and removes all white space in it.
+#Then it prints the new string without spaces.
+
+thing = input("Enter a text here: ")
+
+for f in thing:
+    pass
--- main.py
+++
@@ -1,1 +0,0 @@
-sasdfwsegwer4asfaf
